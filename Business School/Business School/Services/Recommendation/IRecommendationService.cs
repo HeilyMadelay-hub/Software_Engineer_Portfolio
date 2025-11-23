573a74d1de9a1033ff7784a01c4d505aad4c81a5
MadelayHeily-hub <madcodlife@gmail.com>
2025-11-23 01:17:18 +0100

    Update IRecommendationService interface

--- Business School/Business School/Services/Recommendation/IRecommendationService.cs
+++ Business School/Business School/Services/Recommendation/IRecommendationService.cs
@@ -1,15 +1,11 @@
-﻿namespace Business_School.Services.Recommendation
+namespace Business_School.Services.Recommendation
 {
-
-    //The controller asks to the interface for something it needs
-    //and the interface defines what methods are available.
-    //That's why they are always together in th same folder  
-
-
-
-    //This interface declares what methods will implement
-
+    // The controller asks to the interface for something it needs
+    // and the interface defines what methods are available.
+    // That's why they are always together in th same folder 
+    // This interface declares what methods will implement
     public interface IRecommendationService
     {
+      
     }
 }
