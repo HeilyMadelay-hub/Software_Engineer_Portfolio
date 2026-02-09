de856dbf59e136d8c653f97ecea1c9b08256eab1
MadelayHeily-hub <madcodlife@gmail.com>
2026-02-09 23:57:47 +0100

    Fix imports

--- back/src/service/db.ts
+++ back/src/service/db.ts
@@ -1,4 +1,4 @@
-import { Sequelize } from "sequelize";//Esta librearia esta configurada para trabajar con una base de datos SQLite en Node.js
+import { Sequelize } from 'sequelize';//Esta librearia esta configurada para trabajar con una base de datos SQLite en Node.js
 
 //Esto el el ORM que actua como traductor entre el código y la base de datos
 const sequelize = new Sequelize({
